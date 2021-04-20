381525e26eaab601609c1eaeb2c608dff4c9b043
chen <738179359@msn.cn>
2021-04-20 09:51:05 +0800

    最后一次提交

--- src/main-dev.js
+++ src/main-dev.js
@@ -10,7 +10,7 @@ import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
 // 配置请求根路径
 // axios.defaults.baseURL = 'http://47.96.121.235:8080/'
-axios.defaults.baseURL = 'http://192.168.90.7:8080/'
+axios.defaults.baseURL = 'http://47.96.121.235:8080/'
 // axios请求拦截器，请求头必须含有 Authorization ,在 request 拦截器中，展示进度条 Nprogress.start()
 axios.interceptors.request.use(config => {
   NProgress.start()
